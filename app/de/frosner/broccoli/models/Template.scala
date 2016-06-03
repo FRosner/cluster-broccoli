d272f45b030831242d6b89fb45ce80cd3809a7b6
Frank Rosner <frank@fam-rosner.de>
2016-06-03 23:40:40 +0200

    remove unused code

--- app/de/frosner/broccoli/models/Template.scala
+++ app/de/frosner/broccoli/models/Template.scala
@@ -25,9 +25,6 @@ object Template {
 
   val TemplatePattern = Pattern.compile("\\$\\{([A-Za-z][A-Za-z0-9\\_]*)\\}")
 
-  // TODO read templates from existing template files + check that all prefixes match with the defined one (e.g. ds)
-  var templates = Seq(Template("1", "Hallo ${name}"), Template("2", "Hallo ${name}, how is ${object}"))
-
   implicit val templateWrites: Writes[Template] = (
     (JsPath \ "id").write[String] and
       (JsPath \ "template").write[String] and
