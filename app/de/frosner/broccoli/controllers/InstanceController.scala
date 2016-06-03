27983d70127b1987b63babc079a2cb6afe3f91e9
Frank Rosner <frank@fam-rosner.de>
2016-06-04 00:16:41 +0200

    return 404 if instance is not found

--- app/de/frosner/broccoli/controllers/InstanceController.scala
+++ app/de/frosner/broccoli/controllers/InstanceController.scala
@@ -11,13 +11,14 @@ import play.api.mvc.{Action, Controller}
 
 class InstanceController @Inject() (instanceService: InstanceService) extends Controller {
 
+  private val instances = instanceService.instances
 
   def list = Action {
-    Ok(Json.toJson(instanceService.instances))
+    Ok(Json.toJson(instances))
   }
 
   def show(id: String) = Action {
-    Ok(Json.toJson(instanceService.instances.find(_.id == id).get))
+    instances.find(_.id == id).map(template => Ok(Json.toJson(template))).getOrElse(NotFound)
   }
 
 }
