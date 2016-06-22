fcc0b0dfe1d4927507059ab5590a1fc900babf0c
Frank Rosner <frank@fam-rosner.de>
2016-06-22 11:11:09 +0200

    read protocol from tags

--- app/de/frosner/broccoli/services/ConsulService.scala
+++ app/de/frosner/broccoli/services/ConsulService.scala
@@ -12,7 +12,7 @@ import play.api.libs.json._
 import play.api.libs.ws.WSClient
 import play.api.{Configuration, Logger}
 
-import scala.util.{Failure, Success}
+import scala.util.{Failure, Success, Try}
 
 
 // TODO allow asking for services
@@ -35,11 +35,19 @@ class ConsulService @Inject()(configuration: Configuration, ws: WSClient) extend
             val responseServices = responseJsonArray.value.map { serviceJson =>
               val fields = serviceJson.as[JsObject].value
               // TODO proper JSON parsing with exception handling
+              val serviceName = fields("ServiceName").as[JsString].value
+              val serviceProtocol = ConsulService.extractProtocolFromTags(fields("ServiceTags")) match {
+                case Some(protocol) => protocol
+                case None => Logger.warn("Service did not specify a single protocol tag (e.g. protocol:https). Assuming https.")
+                  "https"
+              }
+              val serviceAddress = fields("ServiceAddress").as[JsString].value
+              val servicePort = fields("ServicePort").as[JsNumber].value.toInt
               Service(
-                name = fields("ServiceName").as[JsString].value,
-                protocol = "http", // TODO read from tags
-                address = fields("ServiceAddress").as[JsString].value,
-                port = fields("ServicePort").as[JsNumber].value.toInt
+                name = serviceName,
+                protocol = serviceProtocol,
+                address = serviceAddress,
+                port = servicePort
               )
             }
             sendingService ! ConsulServices(jobId, responseServices)
@@ -54,5 +62,15 @@ class ConsulService @Inject()(configuration: Configuration, ws: WSClient) extend
 
 object ConsulService {
   case class ServiceStatusRequest(jobId: String, serviceNames: Iterable[String])
+
+  def extractProtocolFromTags(tagsJs: JsValue): Option[String] = {
+    val tags = tagsJs.as[JsArray].value.map(_.as[JsString].value)
+    val protocolTags = tags.filter(_.startsWith("protocol:"))
+    val protocols = protocolTags.map(_.split(':')(1))
+    protocols.size match {
+      case 1 => Some(protocols.head)
+      case others => None
+    }
+  }
 }
 
