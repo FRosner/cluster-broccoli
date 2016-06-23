40bba3584b9b6b11d78658fc0fb2b100bc009460
Frank Rosner <frank@fam-rosner.de>
2016-06-23 17:53:56 +0200

    #3 add button for instance deletion

--- public/broccoli.js
+++ public/broccoli.js
@@ -1,80 +1,84 @@
 angular.module('broccoli', ['restangular', 'ui.bootstrap'])
-    .controller('AppsCtrl', function(Restangular, $uibModal, $scope, $timeout) {
-        var vm = this;
-        vm.templates = {};
+  .controller('AppsCtrl', function(Restangular, $uibModal, $scope, $timeout) {
+    var vm = this;
+    vm.templates = {};
 
-        Restangular.setBaseUrl("/api/v1");
+    Restangular.setBaseUrl("/api/v1");
 
-        vm.openModal = openModal;
+    function updateInstances(template) {
+      Restangular.all("instances").getList({ "templateId" : template.id }).then(function(instances) {
+        template.instances = {};
+        instances.forEach(function(instance) {
+          template.instances[instance.id] = instance;
+        });
+      });
+      $timeout(function(){
+        updateInstances(template);
+      }, 1000);
+    }
 
-        updateTemplates();
-
-        function updateInstances(template) {
-          console.log("Updating instance of template " + template.id)
-          Restangular.all("instances").getList({ "templateId" : template.id }).then(function(instances) {
+    function updateTemplates() {
+      console.log("Updating templates")
+      Restangular.all("templates").getList().then(function(templateIds) {
+        templateIds.forEach(function(templateId) {
+          Restangular.one("templates", templateId).get().then(function(template){
+            // TODO include template images later
             template.instances = {};
-            instances.forEach(function(instance) {
-              template.instances[instance.id] = instance;
-            });
-          });
-          $timeout(function(){
+            vm.templates[template.id] = template;
             updateInstances(template);
-          }, 1000);
-        }
-
-        function updateTemplates() {
-          console.log("Updating templates")
-          Restangular.all("templates").getList().then(function(templateIds) {
-            templateIds.forEach(function(templateId) {
-              Restangular.one("templates", templateId).get().then(function(template){
-                // TODO include template images later
-                template.instances = {};
-                vm.templates[template.id] = template;
-                updateInstances(template);
-              });
-            });
           });
+        });
+      });
+    }
+
+    function submitStatus(instance, status) {
+      Restangular.all("instances")
+        .customPOST('"' + status + '"', instance.id, {}, {})
+        .then(function(updatedInstance) {
+          for (i in updatedInstance) {
+            instance[i] = updatedInstance[i];
+          };
+        });
+    }
+
+    function deleteInstance(template, instance) {
+      delete template.instances[instance.id];
+      instance.remove();
+    }
+    vm.deleteInstance = deleteInstance;
+
+    $scope.submitStatus = submitStatus;
+
+    function openModal(templateApp) {
+      var modalInstance = $uibModal.open({
+        animation: true,
+        templateUrl: '/assets/newInstanceModal.html',
+        controller: 'NewInstanceCtrl',
+        controllerAs: 'instCtrl',
+        size: undefined,
+        resolve: {
+          templateId: function () {
+            return templateApp.id;
+          },
+          parameters: function () {
+            return templateApp.parameters;
+          }
         }
+      });
 
-        function submitStatus(instance, status) {
-          Restangular.all("instances")
-            .customPOST('"' + status + '"', instance.id, {}, {})
-            .then(function(updatedInstance) {
-              for (i in updatedInstance) {
-                instance[i] = updatedInstance[i];
-              };
-            });
-        }
+      modalInstance.result.then(function (paramsToValue) {
+        Restangular.all("instances").post({
+          templateId: templateApp.id,
+          parameters: paramsToValue
+        }).then(function(newInstance) {
+          templateApp.instances.push(newInstance);
+        }, function(error) {
+          console.log("There was an error creating");
+          console.log(error);
+        });
+      });
+    };
+    vm.openModal = openModal;
 
-        $scope.submitStatus = submitStatus;
-
-        function openModal(templateApp) {
-          var modalInstance = $uibModal.open({
-            animation: true,
-            templateUrl: '/assets/newInstanceModal.html',
-            controller: 'NewInstanceCtrl',
-            controllerAs: 'instCtrl',
-            size: undefined,
-            resolve: {
-              templateId: function () {
-                return templateApp.id;
-              },
-              parameters: function () {
-                return templateApp.parameters;
-              }
-            }
-          });
-
-          modalInstance.result.then(function (paramsToValue) {
-            Restangular.all("instances").post({
-              templateId: templateApp.id,
-              parameters: paramsToValue
-            }).then(function(newInstance) {
-              templateApp.instances.push(newInstance);
-            }, function(error) {
-              console.log("There was an error creating");
-              console.log(error);
-            });
-          });
-        };
-    });
+    updateTemplates();
+  });
