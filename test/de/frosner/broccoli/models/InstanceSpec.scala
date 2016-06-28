5a361f1185396935c86bc12716de155aedd117c2
Frank Rosner <frank@fam-rosner.de>
2016-06-28 20:17:10 +0200

    #8 add some more InstanceSpec tests

--- test/de/frosner/broccoli/models/InstanceSpec.scala
+++ test/de/frosner/broccoli/models/InstanceSpec.scala
@@ -18,9 +18,19 @@ class InstanceSpec extends Specification {
       Instance("1", Template("1", "\"{{name}}\"", "desc"), Map.empty, InstanceStatus.Unknown, Map.empty) must throwA[IllegalArgumentException]
     }
 
-    "parse the template correctly" in {
+    "parse the template correctly when it contains a single parameter" in {
       val instance = Instance("1", Template("1", "\"{{name}}\"", "desc"), Map("name" -> "Frank"), InstanceStatus.Unknown, Map.empty)
       instance.templateJson === JsString("Frank")
+    }
+
+    "parse the template correctly when it contains multiple parameters" in {
+      val instance = Instance("1", Template("1", "\"{{name}} {{age}}\"", "desc"), Map("name" -> "Frank", "age" -> "5"), InstanceStatus.Unknown, Map.empty)
+      instance.templateJson === JsString("Frank 5")
+    }
+
+    "parse the template correctly when it contains no parameter" in {
+      val instance = Instance("1", Template("1", "\"name\"", "desc"), Map.empty, InstanceStatus.Unknown, Map.empty)
+      instance.templateJson === JsString("name")
     }
 
   }
