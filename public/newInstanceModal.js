92afb3c0166b07682395e1b073c3d905e69fd7c0
pibe81 <pliguori@gmail.com>
2016-08-23 12:08:47 +0200

    #24: Modal popup - focus on first input field

--- public/newInstanceModal.js
+++ public/newInstanceModal.js
@@ -15,4 +15,19 @@ angular.module('broccoli')
     function cancel() {
       $uibModalInstance.dismiss();
     };
+})
+  .directive('focusField', function() {
+  return{
+         restrict: 'A',
+
+         link: function(scope, element, attrs){
+           scope.$watch(function(){
+             return scope.$eval(attrs.focusField);
+             },function (newValue){
+               if (newValue === true){
+                   element[0].focus();
+               }
+           });
+         }
+     };
 });
