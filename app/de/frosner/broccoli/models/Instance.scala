6a1117cd87620f78aa6ae59dc46895ae3a4371b5
Frank Rosner <frank@fam-rosner.de>
2016-09-16 19:49:56 +0200

    #76 simplify instance JSON writes

--- app/de/frosner/broccoli/models/Instance.scala
+++ app/de/frosner/broccoli/models/Instance.scala
@@ -62,18 +62,6 @@ case class Instance(id: String,
 
 object Instance {
 
-  implicit val instanceWrites: Writes[Instance] = (
-    (JsPath \ "id").write[String] and
-      (JsPath \ "parameterValues").write[Map[String, String]] and
-      (JsPath \ "status").write[InstanceStatus] and
-      (JsPath \ "services").write[Map[String, Service]] and
-      (JsPath \ "template").write[Template]
-    )((instance: Instance) => (
-      instance.id,
-      instance.parameterValues,
-      instance.status,
-      instance.services,
-      instance.template
-    ))
+  implicit val instanceWrites: Writes[Instance] = Json.writes[Instance]
 
 }
