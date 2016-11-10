0336f28c1d15393d216379357eb542d68c948cbb
ishmeetkaur <ishmeetkaur77@gmail.com>
2016-11-10 12:25:46 +0100

    Confirmation before instance deletion

--- public/newInstanceModal.js
+++ public/newInstanceModal.js
@@ -1,5 +1,5 @@
 angular.module('broccoli')
-  .controller('NewInstanceCtrl', function ($scope, $rootScope, $uibModalInstance, template, instance, templates) {
+  .controller('NewInstanceCtrl', function ($scope, $rootScope, $uibModalInstance, template, instance, templates, deleteInstance) {
     var vm = this;
     vm.templateId = template.id;
     var realTemplate = null;
@@ -7,15 +7,24 @@ angular.module('broccoli')
     vm.dropdown = {};
     vm.dropdown.templates = templates;
     vm.dropdown.selectedTemplate = "unchanged";
+    vm.deleteInstance =deleteInstance;
 
-    if (instance == null) {
-      vm.panelTitle = "New " + template.id + " (" +  template.version.substring(0, 8) + ")";
-      vm.okText = "Create instance";
-      vm.paramsToValue = {};
-      realTemplate = template;
-    } else {
-      vm.panelTitle = "Edit " + instance.id + " (" + instance.template.id + ", " + instance.template.version.substring(0, 8) + ")";
-      vm.okText = "Edit instance";
+    if (deleteInstance == null){
+      if (instance == null) {
+        vm.panelTitle = "New " + template.id + " (" +  template.version.substring(0, 8) + ")";
+        vm.okText = "Create instance";
+        vm.paramsToValue = {};
+        realTemplate = template;
+      } else {
+        vm.panelTitle = "Edit " + instance.id + " (" + instance.template.id + ", " + instance.template.version.substring(0, 8) + ")";
+        vm.okText = "Edit instance";
+        vm.paramsToValue = instance.parameterValues;
+        realTemplate = instance.template;
+      }
+    }
+    else{
+      vm.panelTitle = "Delete " + instance.id + " (" + instance.template.id + ", " + instance.template.version.substring(0, 8) + ")";
+      vm.okText = "Delete instance";
       vm.paramsToValue = instance.parameterValues;
       realTemplate = instance.template;
     }
@@ -47,11 +56,11 @@ angular.module('broccoli')
     vm.cancel = cancel;
 
     function ok() {
-      Object.keys(vm.paramsToValue).forEach(function(param) {
+        Object.keys(vm.paramsToValue).forEach(function(param) {
         if (vm.paramsToValue[param] == "") {
           delete vm.paramsToValue[param];
         }
-      });
+      }); 
       $uibModalInstance.close({
         "paramsToValue": vm.paramsToValue,
         "instance": instance,
