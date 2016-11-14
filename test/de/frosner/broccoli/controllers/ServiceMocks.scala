dfe6fca82916d52ac989a0cd3dc86eb5a61d3390
Frank Rosner <frank@fam-rosner.de>
2016-11-14 13:57:14 +0100

    #136 check that everything is actually a mock

--- test/de/frosner/broccoli/controllers/ServiceMocks.scala
+++ test/de/frosner/broccoli/controllers/ServiceMocks.scala
@@ -3,10 +3,18 @@ package de.frosner.broccoli.controllers
 import de.frosner.broccoli.conf
 import de.frosner.broccoli.services.{BuildInfoService, InstanceService, PermissionsService, SecurityService}
 import org.mockito.Mockito._
+import org.mockito.internal.util.MockUtil
 
 trait ServiceMocks {
 
+  private val mockUtil = new MockUtil()
+  private def requireMock(obj: AnyRef): Unit = require(
+    mockUtil.isMock(obj),
+    s"${obj.getClass.toString} needs to be a mock!"
+  )
+
   def withAuthConf(securityService: SecurityService, allowed: Iterable[Account]): SecurityService = {
+    requireMock(securityService)
     allowed.foreach { account =>
       when(securityService.getAccount(account.name)).thenReturn(Some(account))
       when(securityService.isAllowedToAuthenticate(account)).thenReturn(true)
@@ -16,11 +24,13 @@ trait ServiceMocks {
   }
 
   def withAuthNone(securityService: SecurityService): SecurityService = {
+    requireMock(securityService)
     when(securityService.authMode).thenReturn(conf.AUTH_MODE_NONE)
     securityService
   }
 
   def withDummyValues(buildInfoService: BuildInfoService): BuildInfoService = {
+    requireMock(buildInfoService)
     when(buildInfoService.projectName).thenReturn("project")
     when(buildInfoService.projectVersion).thenReturn("version")
     when(buildInfoService.scalaVersion).thenReturn("scala")
@@ -29,11 +39,13 @@ trait ServiceMocks {
   }
 
   def withEmptyInstancePrefix(instanceService: InstanceService): InstanceService = {
+    requireMock(instanceService)
     when(instanceService.nomadJobPrefix).thenReturn("")
     instanceService
   }
 
   def withDefaultPermissionsMode(permissionsService: PermissionsService): PermissionsService = {
+    requireMock(permissionsService)
     when(permissionsService.getPermissionsMode()).thenReturn(conf.PERMISSIONS_MODE_DEFAULT)
     permissionsService
   }
