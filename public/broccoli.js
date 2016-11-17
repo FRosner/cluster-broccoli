1b540b4d155805b71e152526530e0a55314c5ad2
Frank Rosner <frank@fam-rosner.de>
2016-11-18 00:35:37 +0100

    #136 progress bar

--- public/broccoli.js
+++ public/broccoli.js
@@ -1,95 +1,109 @@
 angular.module('broccoli', ['restangular', 'ui.bootstrap'])
   .controller('MainController', function(Restangular, AboutService, TemplateService, InstanceService ,$scope, $rootScope, $timeout) {
-      var vm = this;
-      vm.templates = {};
-      vm.about = {};
+    var vm = this;
+    vm.templates = {};
+    vm.about = {};
 
+    $rootScope.broccoliReachable = true;
+    $rootScope.dismissRestangularError = function() {
+        $rootScope.restangularError = null;
+    };
+
+    Restangular.setBaseUrl("/api/v1");
+
+    Restangular.addResponseInterceptor(function(data, operation, what, url, response, deferred) {
       $rootScope.broccoliReachable = true;
-      $rootScope.dismissRestangularError = function() {
-          $rootScope.restangularError = null;
-      };
+      if (url != "/api/v1/status") {
+        $rootScope.isLoggedIn = true;
+      }
+      return data;
+    });
 
-      Restangular.setBaseUrl("/api/v1");
+    Restangular.setErrorInterceptor(function(response, deferred, responseHandler) {
+      if (response.status == -1) {
+        $rootScope.broccoliReachable = false;
+        $rootScope.isLoggedIn = false;
+      } else if (response.status == 403) {
+        $rootScope.isLoggedIn = false;
+      } else if (response.config.url == "/api/v1/auth/login") {
+        $rootScope.restangularError = "Login failed!"
+      } else if (response.config.url == "/api/v1/auth/logout") {
+        $rootScope.restangularError = "Logout failed!"
+      } else {
+        $rootScope.restangularError = response.statusText + " (" + response.status + "): " + response.data;
+      }
+      return false;
+    });
 
-      Restangular.addResponseInterceptor(function(data, operation, what, url, response, deferred) {
-        $rootScope.broccoliReachable = true;
-        if (url != "/api/v1/status") {
-          $rootScope.isLoggedIn = true;
-        }
-        return data;
+    function askStatus() {
+      AboutService.getStatus().then(function(about) {
+        vm.about = about;
       });
+    }
 
-      Restangular.setErrorInterceptor(function(response, deferred, responseHandler) {
-        if (response.status == -1) {
-          $rootScope.broccoliReachable = false;
-          $rootScope.isLoggedIn = false;
-        } else if (response.status == 403) {
-          $rootScope.isLoggedIn = false;
-        } else if (response.config.url == "/api/v1/auth/login") {
-          $rootScope.restangularError = "Login failed!"
-        } else if (response.config.url == "/api/v1/auth/logout") {
-          $rootScope.restangularError = "Logout failed!"
-        } else {
-          $rootScope.restangularError = response.statusText + " (" + response.status + "): " + response.data;
+    function refreshStatus() {
+      Restangular.one("status").get();
+      $timeout(function() {
+        refreshStatus()
+      }, 1000);
+    }
+
+    refreshStatus();
+
+    $rootScope.$watch('isLoggedIn', function(val) {
+      if(val) {
+        $scope.pageLoading = true;
+        function progressTo(i) {
+          return function() {
+            $scope.pageLoadingProgress = i;
+          };
         }
-        return false;
-      });
+        for (i = 0; i <= 100; i = i + 25) {
+          $timeout(progressTo(i), i * 5);
+        }
 
-      function askStatus() {
+        $timeout(function() {
+          $scope.pageLoading = false;
+          progressTo(0);
+        }, 1100);
         AboutService.getStatus().then(function(about) {
           vm.about = about;
         });
+        refreshTemplates();
+      } else {
+        vm.about = {};
       }
+    });
 
-      function refreshStatus() {
-        Restangular.one("status").get();
-        $timeout(function() {
-          refreshStatus()
-        }, 1000);
-      }
+    function refreshTemplates() {
+        TemplateService.getTemplates().then(function(templates) {
+            var templates = templates;
+            templates.forEach(function(template) {
+                template.instances = {};
+                vm.templates[template.id] = template;
+                refreshInstances(template);
+                $scope.templates = templates;
+            });
+        });
+    }
 
-      refreshStatus();
-
-      $rootScope.$watch('isLoggedIn', function(val) {
-        if(val) {
-          AboutService.getStatus().then(function(about) {
-            vm.about = about;
-          });
-          refreshTemplates();
-        } else {
-          vm.about = {};
+    function refreshInstances(template) {
+        InstanceService.getInstances(template).then(function(instances) {
+            $scope.instances = instances;
+            template.instances = {};
+            instances.forEach(function(instance) {
+                template.instances[instance.id] = instance;
+                return template.instances;
+            });
+        });
+        if ($scope.isLoggedIn) {
+          $timeout(function () {
+            refreshInstances(template);
+          }, 1000);
         }
-      });
+    }
 
-      function refreshTemplates() {
-          TemplateService.getTemplates().then(function(templates) {
-              var templates = templates;
-              templates.forEach(function(template) {
-                  template.instances = {};
-                  vm.templates[template.id] = template;
-                  refreshInstances(template);
-                  $scope.templates = templates;
-              });
-          });
-      }
-
-      function refreshInstances(template) {
-          InstanceService.getInstances(template).then(function(instances) {
-              $scope.instances = instances;
-              template.instances = {};
-              instances.forEach(function(instance) {
-                  template.instances[instance.id] = instance;
-                  return template.instances;
-              });
-          });
-          if ($scope.isLoggedIn) {
-            $timeout(function () {
-              refreshInstances(template);
-            }, 1000);
-          }
-      }
-
-      refreshTemplates();
+    refreshTemplates();
   }).directive('focusField', function() {
     return {
       restrict: 'A',
