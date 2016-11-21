26fccffff276cec43d13b8be11fbe729dbfbfd9c
Frank Rosner <frank@fam-rosner.de>
2016-11-21 18:33:12 +0100

    #139 add some missing semicolons

--- public/InstanceService.js
+++ public/InstanceService.js
@@ -8,16 +8,16 @@ angular.module('broccoli')
           return Restangular.all("instances")
           .getList({ "templateId" : template.id });
           }
-        }   
+        };
 
         this.submitStatus=  function (instance, status) {
           return Restangular.all("instances")
             .customPOST({ "status": status }, instance.id, {}, {})        
-          }
+          };
 
         this.deleteInstance = function (template, instance) {
           instance.remove();
-        }
+        };
         
         this.createInstance = function(template , paramsToValue) {
           return Restangular.all("instances")
