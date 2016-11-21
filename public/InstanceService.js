3aa184979b28753b42c965584e913db384d65e5a
Frank Rosner <frank@fam-rosner.de>
2016-11-21 22:54:13 +0100

    #139 refresh instances right after you made a change

--- public/InstanceService.js
+++ public/InstanceService.js
@@ -1,10 +1,20 @@
 angular.module('broccoli')
   .service('InstanceService', function(Restangular, $rootScope, $timeout) {
 
-    function refreshInstances() {
+    function refreshInstancesOnce() {
       Restangular.all("instances").getList().then(function(instances) {
         $rootScope._instances = instances;
       });
+    }
+
+    function refreshInstancesSoon() {
+      $timeout(function () {
+        refreshInstancesOnce();
+      }, 500);
+    }
+    
+    function refreshInstances() {
+      refreshInstancesOnce();
       var pollingFrequency = ($rootScope._pollingFrequency > 1000) ? $rootScope._pollingFrequency : 1000;
       if ($rootScope._isLoggedIn) {
         $timeout(function () {
@@ -15,24 +25,28 @@ angular.module('broccoli')
     this.refreshInstances = refreshInstances;
 
     this.submitStatus=  function (instance, status) {
-      return Restangular.all("instances")
-        .customPOST({ "status": status }, instance.id, {}, {})
-      };
+      refreshInstancesSoon();
+      Restangular.all("instances")
+        .customPOST({ "status": status }, instance.id, {}, {});
+    };
+    
 
     this.deleteInstance = function (template, instance) {
+      refreshInstancesSoon();
       instance.remove();
     };
 
     this.createInstance = function(template , paramsToValue) {
-      return Restangular.all("instances")
-      .post({
-          templateId: template.id,
-          parameters: paramsToValue
-        })
+      refreshInstancesSoon();
+      Restangular.all("instances").post({
+        templateId: template.id,
+        parameters: paramsToValue
+      });
     };
 
     this.editInstance = function(postData, newInstance) {
-      return Restangular.all("instances")
-      .customPOST(postData, newInstance.id, {}, {});
-        };
+      refreshInstancesSoon();
+      Restangular.all("instances")
+        .customPOST(postData, newInstance.id, {}, {});
+    };
   });
