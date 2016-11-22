94f60a9947516f7acf74e1bbfd76af1812aaf517
Frank Rosner <frank@fam-rosner.de>
2016-11-22 15:13:09 +0100

    #116 load jquery before bootstrap also in karma

--- karma.conf.js
+++ karma.conf.js
@@ -17,6 +17,7 @@ module.exports = function(config) {
     files: [
       'node_modules/angular/angular.js',
       'node_modules/angular-mocks/angular-mocks.js',
+      'public/js/jquery.min.js',
       'public/broccoli.js',
       'public/**/*.js',
       'public/tests/**/*.js'
