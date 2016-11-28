e1be659446146e050cb37bada32260df46a70300
Frank Rosner <frank@fam-rosner.de>
2016-11-28 12:27:27 +0100

    plus font-awesome

--- public/elm/webapp/src/Templates/View.elm
+++ public/elm/webapp/src/Templates/View.elm
@@ -4,9 +4,10 @@ import Html exposing (..)
 import Html.Attributes exposing (class)
 import Templates.Messages exposing (..)
 import Templates.Models exposing (..)
-
+import FontAwesome
+import Color
 
 templateList : Templates -> Html Msg
 templateList templates =
   div []
-    (List.map (\t -> div [] [text t.id]) templates)
+    (List.map (\t -> div [] [FontAwesome.plus_circle Color.black 11, text t.id]) templates)
