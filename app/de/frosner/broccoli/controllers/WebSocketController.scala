6060af6c94b2db3b8af53bdcb33c93362e0b41e6
Frank Rosner <frank@fam-rosner.de>
2017-04-07 16:52:44 +0200

    #181 send templates on websocket connect

--- app/de/frosner/broccoli/controllers/WebSocketController.scala
+++ app/de/frosner/broccoli/controllers/WebSocketController.scala
@@ -2,37 +2,44 @@ package de.frosner.broccoli.controllers
 
 import javax.inject.Inject
 
-import de.frosner.broccoli.services.WebSocketService
+import de.frosner.broccoli.services.{TemplateService, WebSocketService}
+import de.frosner.broccoli.services.WebSocketService.Msg
 import de.frosner.broccoli.util.Logging
+import de.frosner.broccoli.models.Template.templateApiWrites
 import play.api.mvc._
 import play.api.libs.iteratee._
+import play.api.libs.json.{JsString, Json}
 
 import scala.concurrent.ExecutionContext.Implicits.global
 
-class WebSocketController @Inject() (webSocketService: WebSocketService) extends Controller with Logging {
+class WebSocketController @Inject() ( webSocketService: WebSocketService
+                                    , templateService: TemplateService
+                                    ) extends Controller with Logging {
 
   // TODO close on logout
   // TODO authentication with WebSocket.tryAccept
   // TODO authorization (how to manage that clients only receive updates they are allowed to)
 
-  def socket = WebSocket.using[String] { request =>
-
-    // TODO create web socket with id and store it here so we can delete it later
+  def socket = WebSocket.using[Msg] { request =>
     val (connectionId, enumerator) = webSocketService.newConnection()
     val connectionLogString = s"$connectionId ($request) from ${request.remoteAddress}"
     Logger.info(s"New connection $connectionLogString")
+    // webSocketService.send(connectionId, "New templates and instances gogo.") only works after the enumerator and in has been passed
+    // so when / where do we send the initial update?
 
-    val in = Iteratee.foreach[String] {
+
+    val in = Iteratee.foreach[Msg] {
         // TODO call the controller methods
       msg =>
-        println(s"$connectionId Received msg: $msg")
+        Logger.debug(s"Received message through $connectionId: $msg")
     }.map { _ =>
       webSocketService.closeConnection(connectionId)
       Logger.info(s"Closed connection $connectionLogString")
     }
 
     // TODO send request to set all instances and templates initially, then the webSocketService.channel will be used for subsequent updates
-    (in, enumerator)
+    val initialEnumerator = Enumerator[Msg](Json.toJson(templateService.getTemplates))
+    (in, initialEnumerator.andThen(enumerator))
   }
 
 }
--- app/de/frosner/broccoli/services/WebSocketService.scala
+++ app/de/frosner/broccoli/services/WebSocketService.scala
@@ -3,19 +3,21 @@ package de.frosner.broccoli.services
 import java.util.UUID
 import javax.inject.{Inject, Singleton}
 
+import de.frosner.broccoli.services.WebSocketService.Msg
 import de.frosner.broccoli.util.Logging
 import play.api.Configuration
 import play.api.libs.iteratee.{Concurrent, Enumerator}
+import play.api.libs.json.JsValue
 
 // http://stackoverflow.com/questions/24576405/broadcasting-messages-in-play-framework-websockets
 @Singleton
 class WebSocketService @Inject() () extends Logging {
 
   @volatile
-  private var connections: Map[String, Concurrent.Channel[String]] = Map.empty
+  private var connections: Map[String, Concurrent.Channel[Msg]] = Map.empty
 
-  def newConnection(): (String, Enumerator[String]) = {
-    val (enumerator, channel) = Concurrent.broadcast[String]
+  def newConnection(): (String, Enumerator[Msg]) = {
+    val (enumerator, channel) = Concurrent.broadcast[Msg]
     val uuid = UUID.randomUUID().toString
     connections = connections.updated(uuid, channel)
     (uuid, enumerator)
@@ -26,11 +28,31 @@ class WebSocketService @Inject() () extends Logging {
       case Some(channel) =>
         channel.eofAndEnd()
         connections = connections - id
-      case None => throw new IllegalArgumentException(s"Connection $id is not in the connection pool: " +
-        s"${connections.keys.mkString(", ")}")
+      case None => throw InvalidWebsocketConnectionException(id, connections.keys)
+    }
+  }
+
+  def broadcast(msg: Msg): Unit = {
+    connections.foreach {
+      case (id, channel) =>
+        Logger.debug(s"Broadcasting to $id: $msg")
+        channel.push(msg)
+    }
+  }
+
+  def send(id: String, msg: Msg): Unit = {
+    connections.get(id) match {
+      case Some(channel) => channel.push(msg)
+      case None => throw InvalidWebsocketConnectionException(id, connections.keys)
     }
   }
 
 //  def send(message: String) = channel.push(message)
+
+}
+
+object WebSocketService {
+
+  type Msg = JsValue
 
 }
