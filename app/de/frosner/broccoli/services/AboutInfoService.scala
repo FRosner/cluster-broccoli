4fabeb6fe812b732c29b747d74da1967b2dc08ae
Frank Rosner <frank@fam-rosner.de>
2017-04-12 17:38:33 +0200

    #181 wrap ws messages and rework the about controller

--- app/de/frosner/broccoli/services/AboutInfoService.scala
+++ app/de/frosner/broccoli/services/AboutInfoService.scala
@@ -4,7 +4,8 @@ import java.io.File
 import javax.inject.{Inject, Singleton}
 
 import de.frosner.broccoli.conf
-import de.frosner.broccoli.models.Template
+import de.frosner.broccoli.models._
+import de.frosner.broccoli.util.Logging
 import play.Logger
 import play.api.Configuration
 
@@ -12,11 +13,34 @@ import scala.io.Source
 import scala.util.Try
 
 @Singleton
-class AboutInfoService {
+class AboutInfoService @Inject() ( nomadService: NomadService
+                                 , consulService: ConsulService
+                                 , securityService: SecurityService
+                                 ) extends Logging {
 
-  val projectName = de.frosner.broccoli.build.BuildInfo.name
-  val projectVersion = de.frosner.broccoli.build.BuildInfo.version
-  val scalaVersion = de.frosner.broccoli.build.BuildInfo.scalaVersion
-  val sbtVersion = de.frosner.broccoli.build.BuildInfo.sbtVersion
+  def aboutInfo(loggedIn: Account) = AboutInfo(
+    project = AboutProject(
+      name = de.frosner.broccoli.build.BuildInfo.name,
+      version = de.frosner.broccoli.build.BuildInfo.version
+    ),
+    scala = AboutScala(
+      version = de.frosner.broccoli.build.BuildInfo.scalaVersion
+    ),
+    sbt = AboutSbt(
+      version = de.frosner.broccoli.build.BuildInfo.sbtVersion
+    ),
+    auth = AboutAuth(
+      enabled = securityService.authMode != conf.AUTH_MODE_NONE,
+      user = AboutUser(
+        name = loggedIn.name,
+        role = loggedIn.role.toString,
+        instanceRegex = loggedIn.instanceRegex
+      )
+    ),
+    services = AboutServices(
+      clusterManager = AboutClusterManager(connected = nomadService.isNomadReachable),
+      serviceDiscovery = AboutServiceDiscovery(connected = consulService.isConsulReachable)
+    )
+  )
 
 }
