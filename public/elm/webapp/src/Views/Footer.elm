b755bfd5381a8f47313c4a4402f9673b10c58c1e
Frank Rosner <frank@fam-rosner.de>
2017-04-19 20:02:15 +0200

    #181 center the footer

--- public/elm/webapp/src/Views/Footer.elm
+++ public/elm/webapp/src/Views/Footer.elm
@@ -10,31 +10,37 @@ view : Maybe AboutInfo -> Html AnyMsg
 view maybeAboutInfo =
   footer [ class "footer" ]
     [ div [ class "container" ]
-      [ p [ class "text-muted" ]
-        [ text
-          ( maybeAboutInfo
-            |> Maybe.map (\i -> i.projectInfo.name)
-            |> Maybe.withDefault "<Project Name>"
-          )
-        , text " "
-        , text
-          ( maybeAboutInfo
-            |> Maybe.map (\i -> i.projectInfo.version)
-            |> Maybe.withDefault "<Project Version>"
-          )
-        , text " (built with Scala "
-        , text
-          ( maybeAboutInfo
-            |> Maybe.map (\i -> i.scalaInfo.version)
-            |> Maybe.withDefault "<Scala Version>"
-          )
-        , text ", SBT "
-        , text
-          ( maybeAboutInfo
-            |> Maybe.map (\i -> i.sbtInfo.version)
-            |> Maybe.withDefault "<SBT Version>"
-          )
-        , text ")"
+      [ div
+        [ class "row" ]
+        [ div
+          [ class "col-md-12" ]
+          [ p [ class "text-muted text-center" ]
+            [ text
+              ( maybeAboutInfo
+                |> Maybe.map (\i -> i.projectInfo.name)
+                |> Maybe.withDefault "<Project Name>"
+              )
+            , text " "
+            , text
+              ( maybeAboutInfo
+                |> Maybe.map (\i -> i.projectInfo.version)
+                |> Maybe.withDefault "<Project Version>"
+              )
+            , text " (built with Scala "
+            , text
+              ( maybeAboutInfo
+                |> Maybe.map (\i -> i.scalaInfo.version)
+                |> Maybe.withDefault "<Scala Version>"
+              )
+            , text ", SBT "
+            , text
+              ( maybeAboutInfo
+                |> Maybe.map (\i -> i.sbtInfo.version)
+                |> Maybe.withDefault "<SBT Version>"
+              )
+            , text ")"
+            ]
+          ]
         ]
       ]
     ]
