8cbf0fa05ac0d8f3efecf871ff48fe042573774f
Frank Rosner <frank@fam-rosner.de>
2017-04-25 10:30:23 +0200

    #181 upgrade webpack

--- public/elm/webapp/webpack.config.js
+++ public/elm/webapp/webpack.config.js
@@ -33,12 +33,12 @@ module.exports = {
       {
         test:    /\.html$/,
         exclude: /node_modules/,
-        loader:  'file?name=[name].[ext]',
+        loader:  'file-loader?name=[name].[ext]',
       },
       {
         test:    /\.elm$/,
         exclude: [/elm-stuff/, /node_modules/],
-        loader:  'elm-webpack',
+        loader:  'elm-webpack-loader',
       },
       {
         test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
