45b8df65bef1baa6a94d9724742e74337b30c9d9
Frank Rosner <frank@fam-rosner.de>
2017-05-02 16:42:22 +0200

    #181 reestablish the websocket connection when logging in again

--- app/de/frosner/broccoli/controllers/SecurityController.scala
+++ app/de/frosner/broccoli/controllers/SecurityController.scala
@@ -33,6 +33,10 @@ case class SecurityController @Inject()
 
   // TODO remove old websocket connections
   def login = Action.async { implicit request =>
+    getSessionId(request).map(id => (id, webSocketService.closeConnection(id))) match {
+      case Some((id, true)) => Logger.info(s"Removing websocket connection of $id due to another login")
+      case _ =>
+    }
     loginForm.bindFromRequest().fold(
       formWithErrors => Future.successful(Results.BadRequest),
       account => {
