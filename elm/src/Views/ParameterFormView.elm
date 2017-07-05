3f10da5d25900a64ec6e92da24d4819f8502b31a
Sebastian Wiesner <swiesner@lunaryorn.com>
2017-07-05 16:53:21 +0200

    Sort parameters by their ID
    Fixed GH-231

--- elm/src/Views/ParameterFormView.elm
+++ elm/src/Views/ParameterFormView.elm
@@ -93,11 +93,18 @@ editView instance templates maybeInstanceParameterForm visibleSecrets maybeRole 
             )
 
 
-{-| Get other parameters, ie, sorted parameters except "id"
+{-| Get other parameters, in alphabetical order.
+
+"Other" parameters are all parameters except "id" which receives special treatment of being shown
+first in all cases.
+
+Note: We sort parameters by their ID not by their user-visible name, so the sort order in the UI
+might seem different.
+
 -}
 getOtherParameters : List String -> List String
 getOtherParameters params =
-    List.filter ((/=) "id") params
+    List.filter ((/=) "id") params |> List.sortBy String.toLower
 
 
 parametersView parametersH instance template maybeInstanceParameterForm visibleSecrets enabled =
