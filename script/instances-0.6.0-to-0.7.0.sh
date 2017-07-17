600bf7cb406b6af9c1ed556a5477cfcf0a072c99
Frank Rosner <frank@fam-rosner.de>
2017-07-17 11:10:58 +0200

    #244 quote variables

--- script/instances-0.6.0-to-0.7.0.sh
+++ script/instances-0.6.0-to-0.7.0.sh
@@ -15,13 +15,13 @@ else
   backupDir="$instanceDir.bak_$(date +%s)"
 
   echo "Backing up $instanceDir to $backupDir."
-  cp -r $instanceDir $backupDir
+  cp -r "$instanceDir" "$backupDir"
   echo "Converting instances format in $instanceDir from Broccoli <0.6.0 to 0.7.0."
-  for instanceFile in $instanceDir/*.json; do
+  for instanceFile in "$instanceDir/*.json"; do
     echo "- Converting $instanceFile"
     tmpInstanceFile="$instanceFile.tmp"
-    cat $instanceFile | jq '.template.parameterInfos = (.template.parameterInfos | with_entries(.value.id = .value.name))' > $tmpInstanceFile
-    mv $tmpInstanceFile $instanceFile
+    cat "$instanceFile" | jq '.template.parameterInfos = (.template.parameterInfos | with_entries(.value.id = .value.name))' > "$tmpInstanceFile"
+    mv "$tmpInstanceFile" "$instanceFile"
   done
 
   echo "Conversion finished. Looks like everything went well."
@@ -29,7 +29,7 @@ else
   echo
   if [[ $REPLY =~ ^[Yy]$ ]]; then
     echo "Deleting $backupDir."
-    rm -rf $backupDir
+    rm -rf "$backupDir"
   else
     echo "Keeping $backupDir for now. You have to delete it manually."
   fi
