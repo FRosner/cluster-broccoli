3394ec98422a2f74f466c7fc7baefa817f4a918e
Aleksandr Sorokoumov <aleksandr.sorokoumov@gmail.com>
2017-07-27 13:54:15 +0200

    Document exceptions that SignalManager can throw

--- server/src/main/scala/de/frosner/broccoli/signal/SignalManager.scala
+++ server/src/main/scala/de/frosner/broccoli/signal/SignalManager.scala
@@ -11,6 +11,8 @@ trait SignalManager {
     * Registers signal handler for the given signal
     * @param signal
     * @param handler
+    * @throws IllegalArgumentException if a signal is already registered or reserved by JDK or OS
+    * @throws UnsupportedOperationException if OS is not supported
     */
   def register(signal: Signal, handler: SignalHandler): Unit
 
