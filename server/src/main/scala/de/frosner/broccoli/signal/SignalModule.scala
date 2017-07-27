7aea358edc7f7a48608d50d6c8db69a3a503852e
Aleksandr Sorokoumov <aleksandr.sorokoumov@gmail.com>
2017-07-27 14:39:41 +0200

    Use configure method instead of provideSignalManager in SignalModule

--- server/src/main/scala/de/frosner/broccoli/signal/SignalModule.scala
+++ server/src/main/scala/de/frosner/broccoli/signal/SignalModule.scala
@@ -1,14 +1,9 @@
 package de.frosner.broccoli.signal
 
-import javax.inject.Singleton
-
-import com.google.inject.{AbstractModule, Provides}
+import com.google.inject.AbstractModule
 import net.codingwell.scalaguice.ScalaModule
 
 class SignalModule extends AbstractModule with ScalaModule {
-  override def configure(): Unit = {}
-
-  @Provides
-  @Singleton
-  def provideSignalManager(): UnixSignalManager = new UnixSignalManager()
+  override def configure(): Unit =
+    bind[SignalManager].to[UnixSignalManager].asEagerSingleton()
 }
