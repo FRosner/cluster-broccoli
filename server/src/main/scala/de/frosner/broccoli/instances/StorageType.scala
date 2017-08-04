dc37aa3ea2af10f1d6707f3695522a40320c4cb5
Aleksandr Sorokoumov <aleksandr.sorokoumov@gmail.com>
2017-08-04 12:48:54 +0200

    #17 Add type annotations in ParameterType and StorageType

--- server/src/main/scala/de/frosner/broccoli/instances/StorageType.scala
+++ server/src/main/scala/de/frosner/broccoli/instances/StorageType.scala
@@ -2,6 +2,8 @@ package de.frosner.broccoli.instances
 
 import enumeratum.{Enum, EnumEntry, PlayJsonEnum}
 
+import scala.collection.immutable
+
 /**
   * Type of the instance storage
   *
@@ -10,7 +12,7 @@ import enumeratum.{Enum, EnumEntry, PlayJsonEnum}
 sealed abstract class StorageType(override val entryName: String) extends EnumEntry
 
 object StorageType extends Enum[StorageType] with PlayJsonEnum[StorageType] {
-  val values = findValues
+  val values: immutable.IndexedSeq[StorageType] = findValues
   case object FileSystem extends StorageType("fs")
   case object CouchDB extends StorageType("couchdb")
 }
--- server/src/main/scala/de/frosner/broccoli/models/ParameterType.scala
+++ server/src/main/scala/de/frosner/broccoli/models/ParameterType.scala
@@ -2,10 +2,12 @@ package de.frosner.broccoli.models
 
 import enumeratum.{Enum, EnumEntry, PlayJsonEnum}
 
+import scala.collection.immutable
+
 sealed trait ParameterType extends EnumEntry with EnumEntry.Lowercase
 
 object ParameterType extends Enum[ParameterType] with PlayJsonEnum[ParameterType] {
-  val values = findValues
+  val values: immutable.IndexedSeq[ParameterType] = findValues
   case object Raw extends ParameterType
   case object String extends ParameterType
 }
