ae27d183c07a184fe57ac02612e2e4ba1d5c02c8
Aleksandr Sorokoumov <aleksandr.sorokoumov@gmail.com>
2017-08-17 12:03:26 +0200

    #17 Make sanitize a top level function

--- server/src/main/scala/de/frosner/broccoli/templates/TemplateRenderer.scala
+++ server/src/main/scala/de/frosner/broccoli/templates/TemplateRenderer.scala
@@ -3,7 +3,7 @@ package de.frosner.broccoli.templates
 import javax.inject.Inject
 
 import de.frosner.broccoli.instances.conf.InstanceConfiguration
-import de.frosner.broccoli.models.{Instance, ParameterType}
+import de.frosner.broccoli.models.{Instance, ParameterInfo, ParameterType}
 import play.api.libs.json.{JsString, JsValue, Json}
 
 /**
@@ -12,33 +12,33 @@ import play.api.libs.json.{JsString, JsValue, Json}
   * @param instanceConfiguration
   */
 class TemplateRenderer @Inject()(instanceConfiguration: InstanceConfiguration) {
+  private def sanitize(parameter: String, value: String, parameterInfos: Map[String, ParameterInfo]) = {
+    val parameterType = parameterInfos
+      .get(parameter)
+      .flatMap(_.`type`)
+      .getOrElse(instanceConfiguration.defaultParameterType)
+    val sanitized = parameterType match {
+      case ParameterType.Raw    => value
+      case ParameterType.String => JsString(value)
+    }
+    sanitized.toString
+  }
+
   def renderJson(instance: Instance): JsValue = {
     val template = instance.template
     val parameterInfos = template.parameterInfos
     val parameterValues = instance.parameterValues
 
-    def sanitize(parameter: String, value: String) = {
-      val parameterType = parameterInfos
-        .get(parameter)
-        .flatMap(_.`type`)
-        .getOrElse(instanceConfiguration.defaultParameterType)
-      val sanitized = parameterType match {
-        case ParameterType.Raw    => value
-        case ParameterType.String => JsString(value)
-      }
-      sanitized.toString
-    }
-
     val templateWithValues = parameterValues.foldLeft(template.template) {
       case (intermediateTemplate, (parameter, value)) =>
-        intermediateTemplate.replaceAllLiterally(s"{{$parameter}}", sanitize(parameter, value))
+        intermediateTemplate.replaceAllLiterally(s"{{$parameter}}", sanitize(parameter, value, parameterInfos))
     }
     val parameterDefaults = parameterInfos.flatMap {
       case (parameterId, parameterInfo) => parameterInfo.default.map(default => (parameterId, default))
     }
     val templateWithDefaults = parameterDefaults.foldLeft(templateWithValues) {
       case (intermediateTemplate, (parameter, value)) =>
-        intermediateTemplate.replaceAllLiterally(s"{{$parameter}}", sanitize(parameter, value))
+        intermediateTemplate.replaceAllLiterally(s"{{$parameter}}", sanitize(parameter, value, parameterInfos))
     }
     Json.parse(templateWithDefaults)
   }
