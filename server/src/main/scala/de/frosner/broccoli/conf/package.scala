39448db04387e8714613b09de52801fada3d1061
Aleksandr Sorokoumov <aleksandr.sorokoumov@gmail.com>
2017-08-21 17:05:43 +0200

    #17 Remove instance storage and polling configuration from the old conf object

--- server/src/main/scala/de/frosner/broccoli/conf/package.scala
+++ server/src/main/scala/de/frosner/broccoli/conf/package.scala
@@ -8,25 +8,9 @@ package object conf {
   val CONSUL_URL_KEY = "broccoli.consul.url"
   val CONSUL_URL_DEFAULT = "http://localhost:8500"
 
-  val INSTANCES_STORAGE_TYPE_KEY = "broccoli.instances.storage.type"
-  val INSTANCES_STORAGE_TYPE_FS = "fs"
-  val INSTANCES_STORAGE_TYPE_COUCHDB = "couchdb"
-  val INSTANCES_STORAGE_TYPE_DEFAULT = INSTANCES_STORAGE_TYPE_FS
-
-  val INSTANCES_STORAGE_FS_URL_KEY = "broccoli.instances.storage.fs.url"
-  val INSTANCES_STORAGE_FS_URL_DEFAULT = "instances"
-
-  val INSTANCES_STORAGE_COUCHDB_URL_KEY = "broccoli.instances.storage.couchdb.url"
-  val INSTANCES_STORAGE_COUCHDB_URL_DEFAULT = "http://localhost:5984"
-  val INSTANCES_STORAGE_COUCHDB_DBNAME_KEY = "broccoli.instances.storage.couchdb.dbName"
-  val INSTANCES_STORAGE_COUCHDB_DBNAME_DEFAULT = "broccoli_instances"
-
   val CONSUL_LOOKUP_METHOD_KEY = "broccoli.consul.lookup"
   val CONSUL_LOOKUP_METHOD_IP = "ip"
   val CONSUL_LOOKUP_METHOD_DNS = "dns"
-
-  val POLLING_FREQUENCY_KEY = "broccoli.polling.frequency"
-  val POLLING_FREQUENCY_DEFAULT = 1
 
   val AUTH_SESSION_TIMEOUT_KEY = "broccoli.auth.session.timeout"
   val AUTH_SESSION_TIMEOUT_DEFAULT = 3600
