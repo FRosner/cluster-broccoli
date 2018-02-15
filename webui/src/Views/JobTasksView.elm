a7983d688d0b79f22474b0b07901ce1d23b83e37
Frank Rosner <frank@fam-rosner.de>
2018-02-15 11:45:38 +0100

    refactoring: factor out resource usage bars

--- webui/src/Views/JobTasksView.elm
+++ webui/src/Views/JobTasksView.elm
@@ -6,10 +6,9 @@ import Models.Resources.LogKind exposing (LogKind(..))
 import Models.Resources.ClientStatus exposing (ClientStatus(ClientComplete))
 import Models.Resources.Allocation exposing (shortAllocationId)
 import Views.Styles as Styles
+import Views.ResourceUsageBar as ResourceUsageBar
 import Html exposing (..)
 import Html.Attributes exposing (..)
-import Filesize
-import Round
 
 
 view : String -> Maybe (List AllocatedTask) -> Bool -> List (Html msg)
@@ -87,11 +86,11 @@ jobAllocationRow jobId index task =
             , td [] [ text task.taskName ]
             , td []
                 [ Maybe.withDefault (text "Unknown")
-                    (Maybe.map2 cpuUsageBar task.resources.cpuUsedMhz task.resources.cpuRequiredMhz)
+                    (Maybe.map2 ResourceUsageBar.cpuUsageBar task.resources.cpuUsedMhz task.resources.cpuRequiredMhz)
                 ]
             , td []
                 [ Maybe.withDefault (text "Unknown")
-                    (Maybe.map2 memoryUsageBar task.resources.memoryUsedBytes task.resources.memoryRequiredBytes)
+                    (Maybe.map2 ResourceUsageBar.memoryUsageBar task.resources.memoryUsedBytes task.resources.memoryRequiredBytes)
                 ]
             , td
                 -- Do not wrap buttons in this cell
@@ -110,70 +109,6 @@ jobAllocationRow jobId index task =
                     ]
                     [ text "stderr" ]
                 ]
-            ]
-
-
-cpuUsageBar : Float -> Float -> Html msg
-cpuUsageBar current required =
-    resourceUsageBar
-        ((Round.round 0 current) ++ " MHz / " ++ (Round.round 0 required) ++ " MHz CPU used")
-        current
-        required
-
-
-memoryUsageBar : Int -> Int -> Html msg
-memoryUsageBar current required =
-    resourceUsageBar
-        ((Filesize.format current) ++ " of " ++ (Filesize.format required) ++ " memory used")
-        (toFloat current)
-        (toFloat required)
-
-
-resourceUsageBar : String -> Float -> Float -> Html msg
-resourceUsageBar tooltip current required =
-    let
-        ratio =
-            current / required
-
-        context =
-            if ratio > 1.0 then
-                "progress-bar-danger"
-            else if ratio >= 0.8 then
-                "progress-bar-warning"
-            else
-                "progress-bar-success"
-    in
-        div
-            [ class "progress"
-            , style
-                [ ( "width", "100px" )
-                , ( "position", "relative" )
-                ]
-            , title tooltip
-            ]
-            [ div
-                [ class "progress-bar"
-                , class context
-                , attribute "role" "progressbar"
-                , attribute "aria-valuemin" "0"
-                , attribute "aria-valuenow" (Round.round 2 current)
-                , attribute "aria-valuemax" (Round.round 2 current)
-                , style
-                    [ ( "text-align", "center" )
-                    , ( "width", (Round.round 0 (100 * (Basics.min 1.0 ratio))) ++ "%" )
-                    ]
-                ]
-                []
-            , span
-                [ style
-                    [ ( "position", "absolute" )
-                    , ( "left", "0" )
-                    , ( "width", "100%" )
-                    , ( "text-align", "center" )
-                    , ( "z-index", "2" )
-                    ]
-                ]
-                [ text (Round.round 0 (ratio * 100)), text "%" ]
             ]
 
 
