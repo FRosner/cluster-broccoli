f0670e43a3e8d0c4a0d1276cb234dc2aec9d7836
Aleksandr Sorokoumov <aleksandr.sorokoumov@gmail.com>
2018-02-22 14:16:24 +0100

    Construct only one JinjavaConfig object for the JinjavaConfiguration case class

--- server/src/main/scala/de/frosner/broccoli/templates/jinjava/JinjavaConfiguration.scala
+++ server/src/main/scala/de/frosner/broccoli/templates/jinjava/JinjavaConfiguration.scala
@@ -1,13 +1,18 @@
 package de.frosner.broccoli.templates.jinjava
 
 import com.hubspot.jinjava.JinjavaConfig
+import JinjavaConfiguration.defaultJinJavaConfig
 
 final case class JinjavaConfiguration(
-    maxRenderDepth: Int = new JinjavaConfig().getMaxRenderDepth,
-    trimBlocks: Boolean = new JinjavaConfig().isTrimBlocks,
-    lstripBlocks: Boolean = new JinjavaConfig().isLstripBlocks,
-    readOnlyResolver: Boolean = new JinjavaConfig().isReadOnlyResolver,
-    enableRecursiveMacroCalls: Boolean = new JinjavaConfig().isEnableRecursiveMacroCalls,
-    failOnUnknownTokens: Boolean = new JinjavaConfig().isFailOnUnknownTokens,
-    maxOutputSize: Long = new JinjavaConfig().getMaxOutputSize,
-    nestedInterpretationEnabled: Boolean = new JinjavaConfig().isNestedInterpretationEnabled)
+    maxRenderDepth: Int = defaultJinJavaConfig.getMaxRenderDepth,
+    trimBlocks: Boolean = defaultJinJavaConfig.isTrimBlocks,
+    lstripBlocks: Boolean = defaultJinJavaConfig.isLstripBlocks,
+    readOnlyResolver: Boolean = defaultJinJavaConfig.isReadOnlyResolver,
+    enableRecursiveMacroCalls: Boolean = defaultJinJavaConfig.isEnableRecursiveMacroCalls,
+    failOnUnknownTokens: Boolean = defaultJinJavaConfig.isFailOnUnknownTokens,
+    maxOutputSize: Long = defaultJinJavaConfig.getMaxOutputSize,
+    nestedInterpretationEnabled: Boolean = defaultJinJavaConfig.isNestedInterpretationEnabled)
+
+object JinjavaConfiguration {
+  val defaultJinJavaConfig = new JinjavaConfig()
+}
