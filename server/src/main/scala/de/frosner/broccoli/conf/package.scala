5cfc5093b22e4b3feca47ee334b65e762334b00c
Sohaib Iftikhar <sohaib1692@gmail.com>
2019-02-08 09:34:29 +0100

    Made domain-path configurable for consul domain lookup

--- server/src/main/scala/de/frosner/broccoli/conf/package.scala
+++ server/src/main/scala/de/frosner/broccoli/conf/package.scala
@@ -8,6 +8,8 @@ package object conf {
   val CONSUL_URL_DEFAULT = "http://localhost:8500"
 
   val CONSUL_LOOKUP_METHOD_KEY = "broccoli.consul.lookup"
+  val CONSUL_DOMAIN_URL_KEY = "broccoli.consul.domain-url"
+  val CONSUL_DOMAIN_PATH_KEY = "broccoli.consul.domain-path"
   val CONSUL_LOOKUP_METHOD_IP = "ip"
   val CONSUL_LOOKUP_METHOD_DNS = "dns"
 
